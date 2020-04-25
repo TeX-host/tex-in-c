5226cc0f9622272d69d26655715ec9028b167c03
woclass <inkydragon@users.noreply.github.com>
2020-04-25 12:28:33 +0800

    [tex_inc.h] 注释使用 doxygen + md 语法

--- tex_inc.h
+++ tex_inc.h
@@ -1,44 +1,48 @@
-#include "global_const.h" // tt_STAT
 #pragma once
-// 存放仅在 tex.c 中使用的宏
 #ifndef _INC_TEX_ONLY
+/// 存放仅在 tex.c 中使用的宏
 #define _INC_TEX_ONLY
+#include "global_const.h" // tt_STAT
 
-// p24#54: On-line and off-line printing
-// enum Selector = [16, 21]
-//     use by `selector`
+/// [p24#54]: On-line and off-line printing.
+/// `enum Selector` = [16, 21], 
+///     use by #selector
 enum Selector {
-    NO_PRINT = 16, // doesn’t print at all
-    TERM_ONLY,     // prints only on the terminal
-    LOG_ONLY,      // prints only on the transcript file
-    TERM_AND_LOG,  // normal `selector` setting
-    // puts output into a cyclic buffer that is used by the show context routine
-    PSEUDO,
-    // appends the output to the current string in the string pool
-    NEW_STRING,    
-};
-// highest selector setting
-// NOTE: _not_use_, only used in assert
+    NO_PRINT = 16, ///< doesn't print at all
+    TERM_ONLY,     ///< prints only on the terminal
+    LOG_ONLY,      ///< prints only on the transcript file
+    TERM_AND_LOG,  ///< normal #selector setting.
+
+    /// puts output into a cyclic buffer that is used by the show context
+    /// routine
+    PSEUDO = 20,
+    /// appends the output to the current string in the string pool
+    NEW_STRING,
+}; // [p24#54] enum Selector
+
+/// highest selector setting.
+/// NOTE: _not_use_, only used in assert
 #define MAX_SELECTOR NEW_STRING 
 
 
 #ifdef tt_STAT
-/// #121
-#define FREE_AVAIL(x)                                                          \
+/// [p45#121] single-word node liberation.
+/// a one-word node is recycled by calling FREE_AVAIL.
+#define FREE_AVAIL(x) \
     (mem[(x)].hh.rh = avail, avail = (x), dyn_used -= CHAR_NODE_SIZE)
 // end #define FREE_AVAIL(x)
 
-/// #122
-#define FAST_GET_AVAIL(x)                                                      \
-    do {                                                                       \
-        if (((x) = avail)) {                                                   \
-            type(x) = charnodetype;                                            \
-            avail = link(x);                                                   \
-            link(x) = 0;                                                       \
-            dyn_used += CHAR_NODE_SIZE;                                           \
-        } else {                                                               \
-            (x) = get_avail();                                                 \
-        }                                                                      \
+/// [#122] avoid get_avail() if possible, to save time.
+#define FAST_GET_AVAIL(x)               \
+    do {                                \
+        if (((x) = avail)) {            \
+            type(x) = charnodetype;     \
+            avail = link(x);            \
+            link(x) = 0;                \
+            dyn_used += CHAR_NODE_SIZE; \
+        } else {                        \
+            (x) = get_avail();          \
+        }                               \
     } while (0)
 // end #define FAST_GET_AVAIL(x)
 #else
@@ -46,36 +50,38 @@ enum Selector {
 
 #define FREE_AVAIL(x) (mem[(x)].hh.rh = avail, avail = (x))
 
-#define FAST_GET_AVAIL(x)                                                      \
-    do {                                                                       \
-        if (((x) = avail)) {                                                   \
-            type(x) = charnodetype;                                            \
-            avail = link(x);                                                   \
-            link(x) = 0;                                                       \
-        } else {                                                               \
-            (x) = get_avail();                                                 \
-        }                                                                      \
+#define FAST_GET_AVAIL(x)           \
+    do {                            \
+        if (((x) = avail)) {        \
+            type(x) = charnodetype; \
+            avail = link(x);        \
+            link(x) = 0;            \
+        } else {                    \
+            (x) = get_avail();      \
+        }                           \
     } while (0)
 // end #define FAST_GET_AVAIL(x)
 #endif // #121,122: tt_STAT
 
 
-/// p145#371
-#define STORE_NEW_TOKEN(p, x)                                                  \
-    do {                                                                       \
-        int q = get_avail();                                                   \
-        link(p) = q;                                                           \
-        info(q) = (x);                                                         \
-        p = q;                                                                 \
+/// [p145#371]
+#define STORE_NEW_TOKEN(p, x) \
+    do {                      \
+        int q = get_avail();  \
+        link(p) = q;          \
+        info(q) = (x);        \
+        p = q;                \
     } while (0)
 // end #define STORE_NEW_TOKEN(p, x)
-#define FAST_STORE_NEW_TOKEN(p, x)                                             \
-    do {                                                                       \
-        int q;                                                                 \
-        FAST_GET_AVAIL(q);                                                     \
-        link(p) = q;                                                           \
-        info(q) = (x);                                                         \
-        p = q;                                                                 \
+
+/// [p145#371]
+#define FAST_STORE_NEW_TOKEN(p, x) \
+    do {                           \
+        int q;                     \
+        FAST_GET_AVAIL(q);         \
+        link(p) = q;               \
+        info(q) = (x);             \
+        p = q;                     \
     } while (0)
 // end #define FAST_STORE_NEW_TOKEN(p, x)
 
