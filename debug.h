cf88f57afce6e5ca45d5eb3d0b3df9a6c4dc6367
woclass <inkydragon@users.noreply.github.com>
2020-10-02 23:39:31 +0800

    [main_ctrl] 移动宏定义到模块

--- debug.h
+++ debug.h
@@ -3,6 +3,14 @@
 /// [#1338] Debugging.
 #define INC_DEBUG_H
 
+/** @addtogroup S110x114_P42x43
+ * @{
+ */
+/// [p470#1338] label _not_use_
+// #define breakpoint 888
+/** @}*/ // end group S1338x1339_P470x471
+
+
 extern void debughelp(void);
 
 #endif /* INC_DEBUG_H */
--- tex.h
+++ tex.h
@@ -8,8 +8,6 @@
 #include "tex_constant.h"
 
 
-#define breakpoint 888
-
 // inputln, [tex]
 extern jmp_buf _JMP_global__end_of_TEX;
 extern jmp_buf _JMP_global__final_end;
